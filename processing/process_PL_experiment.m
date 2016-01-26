b0803bce6349be1e135171d81076559258d1147c
TJ Florence <tj.florence@gmail.com>
2016-01-26 10:36:52 -0500

    more processing

--- processing/process_PL_experiment.m
+++ processing/process_PL_experiment.m
@@ -1,34 +1,266 @@
-function process_PL_experiment
+function process_PL_experiment(expdir)
 
-expdir = '/Users/florencet/Documents/matlab_root/chrap-project/meeting_and_analysis/20160120/20160119/201601190046_HCxUAS-Chr_chrap-PL';
 
 cd(expdir)
 
-tfiles = dir('trial*');
+load('metadata.mat')
 
-for ii = 1:length(tfiles)
-   
-    load(tfiles(ii).name)
+if exp.completed == 0
+    cd('..')
+    rmdir(expdir, 's')
+else
     
+    tfiles = dir('trial*');
+    
+    summary_data.mean_speed = nan(1,11);
+    summary_data.time_to_target = nan(1,11);
+    summary_data.time_at_target = nan(1,11);
+    summary_data.quad_pref = nan(1,11);
+    summary_data.dir_idx = nan(1,11);
+    summary_data.PLI_30 = nan;
+    summary_data.PLI_60 = nan;
+
+    for ii = 1:length(tfiles)
+   
+        load(tfiles(ii).name)
+        trial = calc_tspeed(trial, exp);
+        trial = calc_time_to_target(trial, exp);
+        trial = calc_time_at_target(trial, exp);
+        trial = calc_quad_pref(trial, exp);
+        trial = calc_direction_idx(trial, exp);
+        
+        summary_data.mean_speed(ii) = nanmean(trial.data.speed(trial.data.speed>5));
+        summary_data.time_to_target(ii) = trial.data.time_to_target;
+        summary_data.time_at_target(ii) = trial.data.time_at_target;
+        summary_data.quad_pref(ii) = trial.data.quad_pref_full;
+        summary_data.dir_idx(ii) = trial.data.dir_idx;
+        
+        
+        if ii == 11
+           
+            summary_data.PLI_30 = trial.data.quad_pref_30;
+            summary_data.PLI_60 = trial.data.quad_pref_60;
+            
+        end
+        
+        
+        save([trial.name '.mat'], 'trial', '-v7.3')
+    
+    end
+    
+    save('summary_data.mat', 'summary_data', '-v7.3')
+
+end
+
+
+end
+
+  
+
+function trial = calc_tspeed(trial, exp)
+    
+    mm_per_pix = 0.2073;
+    
+    x = trial.data.xy_filt(:,1);
+    y = trial.data.xy_filt(:,2);
+    t = trial.data.tstamp;
+    
+    x_diff = x(2:end)-x(1:end-1);
+    y_diff = y(2:end)-y(1:end-1);
+    t_diff = t(2:end)-t(1:end-1);
+    
+    dist = sqrt((x_diff.^2) + (y_diff.^2));
+    
+    dist_filt = medfilt1(dist, 11);
+    
+    trial.data.speed = (dist_filt./t_diff)*mm_per_pix;
+    
+     
+end
+
+function trial = calc_time_to_target(trial, exp)
+
+    c_power = exp.settings.stim_power;
+    
+    at_target_ind = find(trial.data.light_power < c_power, 1, 'first');
+    if ~isempty(at_target_ind)
+        trial.data.time_to_target = trial.data.tstamp(at_target_ind);
+    else
+        trial.data.time_to_target = trial.settings.trial_time;
+    end
+
+end
+
+function trial = calc_time_at_target(trial, exp)
+    
+    c_power = exp.settings.stim_power;
+
+    hz = trial.data.p/trial.data.tstamp(trial.data.p);
+    cool_inds = find(trial.data.light_power < c_power);
+    
+    if ~isempty(cool_inds)
+        trial.data.time_at_target = numel(cool_inds)/hz;
+    else
+        trial.data.time_at_target = 0;
+    end
     
 end
 
-    function trial = kfilt_trajectory(trial)
+function trial = calc_quad_pref(trial, exp)
+
+    split_1 = strsplit(trial.name, 'quad');
+    split_2 = strsplit(split_1{2}, 'd');
+    
+    target_Q = str2num(split_2{2});
+  
+    % first do for whole trial
+    x_vals = trial.data.xy_filt(:,1);
+    y_vals = trial.data.xy_filt(:,2);
+    
+    if target_Q == 1
+        
+        ind_t = find(x_vals < 600 & y_vals < 600);
+        ind_d = find(x_vals > 600 & y_vals > 600);
+        
+    elseif target_Q == 2
+        
+        ind_t = find(x_vals > 600 & y_vals < 600);
+        ind_d = find(x_vals < 600 & y_vals > 600);
+        
+    elseif target_Q == 3
+        
+        ind_t = find(x_vals > 600 & y_vals > 600);
+        ind_d = find(x_vals < 600 & y_vals < 600);
+        
+    elseif target_Q == 4
+        
+        ind_d = find(x_vals > 600 & y_vals < 600);
+        ind_t = find(x_vals < 600 & y_vals > 600);
+        
+    end
+
+        
+     trial.data.quad_pref_full = (numel(ind_t)-numel(ind_d))...
+                                /(numel(ind_t)+numel(ind_d));
+                            
+                            
+     % now find at 30 sec and 60 sec
+     ind_30 = find(trial.data.tstamp> 30, 1, 'first');
+     
+    x_vals = trial.data.xy_filt(1:ind_30,1);
+    y_vals = trial.data.xy_filt(1:ind_30,2);
+    
+    if target_Q == 1
+        
+        ind_t = find(x_vals < 600 & y_vals < 600);
+        ind_d = find(x_vals > 600 & y_vals > 600);
+        
+    elseif target_Q == 2
+        
+        ind_t = find(x_vals > 600 & y_vals < 600);
+        ind_d = find(x_vals < 600 & y_vals > 600);
+        
+    elseif target_Q == 3
+        
+        ind_t = find(x_vals > 600 & y_vals > 600);
+        ind_d = find(x_vals < 600 & y_vals < 600);
+        
+    elseif target_Q == 4
+        
+        ind_d = find(x_vals > 600 & y_vals < 600);
+        ind_t = find(x_vals < 600 & y_vals > 600);
+        
+    end
+
+        
+     trial.data.quad_pref_30 = (numel(ind_t)-numel(ind_d))...
+                                /(numel(ind_t)+numel(ind_d));
+                            
+    % now at 60 seconds 
+    ind_60 = find(trial.data.tstamp> 60, 1, 'first');
+     
+    x_vals = trial.data.xy_filt(1:ind_60,1);
+    y_vals = trial.data.xy_filt(1:ind_60,2);
+    
+    if target_Q == 1
+        
+        ind_t = find(x_vals < 600 & y_vals < 600);
+        ind_d = find(x_vals > 600 & y_vals > 600);
+        
+    elseif target_Q == 2
+        
+        ind_t = find(x_vals > 600 & y_vals < 600);
+        ind_d = find(x_vals < 600 & y_vals > 600);
+        
+    elseif target_Q == 3
+        
+        ind_t = find(x_vals > 600 & y_vals > 600);
+        ind_d = find(x_vals < 600 & y_vals < 600);
+        
+    elseif target_Q == 4
+        
+        ind_d = find(x_vals > 600 & y_vals < 600);
+        ind_t = find(x_vals < 600 & y_vals > 600);
+        
+    end
+
+        
+     trial.data.quad_pref_60 = (numel(ind_t)-numel(ind_d))...
+                                /(numel(ind_t)+numel(ind_d));
+                            
+    
+end
+
+function trial = calc_direction_idx(trial, exp)
+
+    split_1 = strsplit(trial.name, 'quad');
+    split_2 = strsplit(split_1{2}, 'd');
+    
+    target_Q = str2num(split_2{2});
+  
+    % first do for whole trial
+    x_vals = trial.data.xy_filt(:,1);
+    y_vals = trial.data.xy_filt(:,2);
+    
+    if target_Q == 1
+        
+        ind_t = find(x_vals < 600 & y_vals < 600);
+        ind_d = find(x_vals > 600 & y_vals > 600);
+        
+    elseif target_Q == 2
+        
+        ind_t = find(x_vals > 600 & y_vals < 600);
+        ind_d = find(x_vals < 600 & y_vals > 600);
+        
+    elseif target_Q == 3
+        
+        ind_t = find(x_vals > 600 & y_vals > 600);
+        ind_d = find(x_vals < 600 & y_vals < 600);
+        
+    elseif target_Q == 4
+        
+        ind_d = find(x_vals > 600 & y_vals < 600);
+        ind_t = find(x_vals < 600 & y_vals > 600);
+        
+    end
+
+    if isempty(ind_t)
+        ind_t = Inf;
+    end
+    
+    if isempty(ind_d)
+        ind_d = Inf;
+    end
+    
+    if ind_t(1) < ind_d(1) 
+        trial.data.dir_idx = 1;
+    elseif ind_t(1) > ind_d(1) 
+        trial.data.dir_idx = -1;
+    else
+         trial.data.dir_idx = 0;
+    end
+        
         
 
-    end
-
-    function calc_tspeed
-    end
-
-    function calc_time_to_target
-    end
-
-
-    function calc_time_at_target
-    end
-
-    function calc_quad_pref
-    end
-    
 end
+    
